58348d443bdd320945aae81308d4420774c8b971
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-10-16 16:20:13 +0200

    Write and read post data to/from passthrough circuit.

--- src/test/scala/PassthroughSpec.scala
+++ src/test/scala/PassthroughSpec.scala
@@ -1,10 +1,14 @@
 package nl.tudelft.post_processor
 
+import TydiPackaging.{FromTydiBinary, TydiBinary, TydiStream}
+import TydiPackaging.FromTydiBinary._
+import TydiPackaging.CustomBinaryConversions._
 import chisel3._
 import chisel3.util.DecoupledIO
-import chiseltest.{ChiselScalatestTester, decoupledToDriver, parallel, testableClock}
+import chiseltest._
 import nl.tudelft.tydi_chisel._
 import org.scalatest.flatspec.AnyFlatSpec
+import PostTestUtils._
 
 class PassthroughSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "passthrough"
@@ -25,12 +29,26 @@ class PassthroughSpec extends AnyFlatSpec with ChiselScalatestTester {
   }
 
   it should "pass through and reconstruct" in {
-    test(new TydiPassthroughModule(new PostStreamGroup(), 1, 264.W)) { c =>
+    val globalWidth = 264
+
+    test(new TydiPassthroughModule(new PostStreamGroup(), 1, globalWidth.W)) { c =>
       c.in.initSource()
       c.out.initSink()
 
-      c.out.expectInvalid()
-      c.clock.step()
+      val postStream = PostTestUtils.getPhysicalStreamsBinary
+      c.out.ready.poke(true.B)
+
+      val passedData = postStream.posts.map( p => {
+        c.in.enqueueNow(p.data.U)
+        val output = c.out.bits.peek()
+        c.clock.step()
+        output
+      })
+
+      val passedLiterals = passedData.map(v => TydiBinary(v.litValue, globalWidth))
+      // val postDebinarizer = FromTydiBinary.gen[Post]
+      val reconstructed = TydiStream.fromBinaryBlobs[Post](passedLiterals, 1)
+      printPosts(reconstructed.toSeq.toList)
     }
   }
 }
