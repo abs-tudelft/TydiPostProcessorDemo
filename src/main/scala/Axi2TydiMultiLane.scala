c7eed8c3ec9b4791db3625f9bb59eda325bba0c1
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-10-22 15:30:38 +0200

    Create multi-lane versions of AXI↔Tydi converters.

---
+++ src/main/scala/Axi2TydiMultiLane.scala
@@ -0,0 +1,36 @@
+package nl.tudelft.post_processor
+
+import chisel3._
+import chisel3.util._
+import nl.tudelft.tydi_chisel.{PhysicalStream, TydiModule}
+
+class Axi2TydiMultiLane(sourceWidth: Int, sink: PhysicalStream) extends TydiModule {
+  private val n = sink.n
+  private val axiWidth = sourceWidth * n
+  private val d = sink.d
+  val input: DecoupledIO[UInt] = IO(Flipped(DecoupledIO(UInt(axiWidth.W))))
+  val output: PhysicalStream = IO(sink.cloneType)
+
+  private val inputVec = WireInit(
+    VecInit.tabulate(n) { i => input.bits((i+1) * sourceWidth - 1, i * sourceWidth) }
+  )
+  private val outputDataVec = Wire(Vec(n, UInt(output.elWidth.W)))
+  output.data := outputDataVec.asUInt
+  private val outputLastVec = Wire(Vec(n, UInt(d.W)))
+  output.last := outputLastVec.asUInt
+  private val outputStrbVec = Wire(Vec(n, Bool()))
+  output.strb := outputStrbVec.asUInt
+
+  input.ready := output.ready
+  output.valid := input.valid
+
+  inputVec.lazyZip(outputDataVec).lazyZip(outputLastVec).lazyZip(outputStrbVec).foreach { case (in, outData, outLast, outStrb) =>
+    outStrb := input.bits(0)
+    outLast := input.bits(sink.d, 1)
+    outData := in(sink.elWidth + sink.d, sink.d + 1)
+  }
+
+  output.user := DontCare
+  output.stai := 0.U
+  output.endi := (n-1).U
+}
---
+++ src/main/scala/Tydi2AxiMultiLane.scala
@@ -0,0 +1,34 @@
+package nl.tudelft.post_processor
+
+import chisel3._
+import chisel3.util._
+import nl.tudelft.tydi_chisel.{PhysicalStream, TydiModule}
+
+class Tydi2AxiMultiLane(sinkWidth: Int, source: PhysicalStream) extends TydiModule {
+  private val n = source.n
+  private val axiWidth = sinkWidth * n
+  private val d = source.d
+  val input: PhysicalStream = IO(Flipped(source.cloneType))
+  val output: DecoupledIO[UInt] = IO(DecoupledIO(UInt(axiWidth.W)))
+
+  private val elWidth = input.elWidth
+  private val inputDataVec = WireInit(
+    VecInit.tabulate(n) { i => input.data((i+1) * elWidth - 1, i * elWidth) }
+  )
+  private val inputLastVec = WireInit(
+    VecInit.tabulate(n) { i => input.last((i+1) * d - 1, i * d) }
+  )
+  private val inputStrbVec = WireInit(
+    VecInit.tabulate(n) { i => input.strb(i) }
+  )
+  private val outputVec = Wire(Vec(n, UInt(sinkWidth.W)))
+  output.bits := outputVec.asUInt
+
+  inputDataVec.lazyZip(inputLastVec).lazyZip(inputStrbVec).zip(outputVec).foreach { case ((data, last, strb), out) =>
+    out := Cat(data, last, strb)
+  }
+
+  input.ready := output.ready
+  output.valid := input.valid
+  input.user := DontCare
+}
