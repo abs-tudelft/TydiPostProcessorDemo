b48980cd6a4699a52a57856a9f7d1d35aa3074f1
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-12-11 17:07:18 +0100

    Change post processor structure, convert from/to AXI directly.
    This way, Tydi streams can be used much easier. And passthrough is not the main thing we want here.

--- src/main/scala/active/PostProcessorMultiLane.scala
+++ src/main/scala/active/PostProcessorMultiLane.scala
@@ -8,34 +8,31 @@ import nl.tudelft.tydi_chisel._
 
 class PostProcessorMultiLane(laneCounts: InputStreamsAsFields[Int]) extends TydiModule with SimpleTypedIO[InputAxiBundle, OutputAxiBundle] {
   val in: InputAxiBundle = IO(Flipped(new InputAxiBundle(laneCounts)))
+  private val inTydi: InputTydiPhysicalBundle = Wire(new InputTydiPhysicalBundle)
   val out: OutputAxiBundle = IO(new OutputAxiBundle(laneCounts))
+  private val outTydi: OutputTydiPhysicalBundle = Wire(new OutputTydiPhysicalBundle)
 
-  private val physicalStreams = new InputTydiPhysicalBundle
+  // Create converters from AXI to Tydi and connect signals.
+  private val inConverters: Seq[Axi2TydiMultiLane] = in.asList.zip(inTydi.asList).map { case (axi, tydi) =>
+    val converter = Module(new Axi2TydiMultiLane(axi.bits.getWidth / tydi.n, tydi))
+    converter.input <> axi
+    tydi := converter.output
+    converter
+  }
 
-  private val laneCountValues = laneCounts.toMap.values.toSeq
+  // Create converters from Tydi to Axi and connect signals.
+  private val outConverters: Seq[Tydi2AxiMultiLane] = out.asList.zip(outTydi.asList).map { case (axi, tydi) =>
+    val converter = Module(new Tydi2AxiMultiLane(axi.bits.getWidth / tydi.n, tydi))
+    converter.input := tydi
+    axi <> converter.output
+    converter
+  }
 
-  private val passthroughs: Seq[TydiPassthroughMultiLane[TydiEl]] = in.asList.lazyZip(physicalStreams.asList).lazyZip(laneCountValues).tail.map { case (axi, tydi, n) =>
-    Module(new TydiPassthroughMultiLane(tydi.elementType, tydi.d, n, axi.bits.getWidth/n))
-  }.toSeq
+  inTydi.asList.tail.zip(outTydi.asList.tail).foreach { case (inTydiLane, outTydiLane) =>
+    outTydiLane := inTydiLane
+  }
 
   private val metadataAdder = Module(new PostMetadataAddition)
-  // Fixme: This process is really inconvenient, must find a way to make all streams available automatically.
-  private val postStream = Wire(PhysicalStream(new PostStreamGroup, 1, 1, 1))
-  private val postWithMetadataStream = Wire(PhysicalStream(new PostWithMetadata, 1, 1, 1))
-  private val inConverter = Module(new Axi2TydiMultiLane(in.posts.getWidth, postStream))
-  private val outConverter = Module(new Tydi2AxiMultiLane(out.posts.getWidth, postWithMetadataStream))
-  inConverter.input <> in.posts
-  postStream := inConverter.output
-  metadataAdder.in := postStream
-  postWithMetadataStream := metadataAdder.out
-  outConverter.input := postWithMetadataStream
-  out.posts <> outConverter.output
-
-  in.asList.tail.zip(passthroughs).foreach { case (io, module) =>
-    module.in <> io
-  }
-
-  out.asList.tail.zip(passthroughs).foreach { case (io, module) =>
-    io <> module.out
-  }
+  metadataAdder.in := inTydi.posts
+  outTydi.posts := metadataAdder.out
 }
